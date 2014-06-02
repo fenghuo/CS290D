ec178508e249da8dc072232f2565b246f4529ab1
fenghuo <372615958@qq.com>
2014-06-01 17:24:57 -0700

    t

--- src/weibo/Weibo.java
+++ src/weibo/Weibo.java
@@ -11,26 +11,29 @@ public class Weibo {
 			
 		//Engine.getDistance(100, "3705342694877442.msgpack", "3705342694877442.msgpack");
 		
-		Engine.Max=6000;
+		Engine.Max=3000;
 		int treeSize=100;
 		int timeLimit=60*60;
 		boolean ignore=false;
-		boolean isSample=true;
+		boolean isSample=false;
 		
 		utils.Data data=null;
 		
 		//int[][]dis=Engine.getPairDistance(utils.path, 100);
-		//data=Engine.getPairDistance(utils.path, treeSize,timeLimit,ignore,isSample);
+		data=Engine.getPairDistance(utils.path, treeSize,timeLimit,ignore,isSample);
 		
 		//utils.dump(data, "2.2000");
 		
-		data=utils.readDump("1.100");
+		//data=utils.readDump("1.100");
 		
-		utils.save(data,"all-100-false-false");
+		utils.save(data,"1000-100-false-false");
+		//data=utils.readSave("all-100-false-false");
 		
-		utils.print(data.data);
+		MDS.run(data.data,data.name);
+
+		//utils.print(data.data);
 	
-		Clustering.Hac(data);
+		//Clustering.Hac(data);
 		
 		return;
 	}
